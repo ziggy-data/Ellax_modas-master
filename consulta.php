f35ed3113b012934d4450da752e231239217dfa0
Reinaldo <reinaldo.ab00@gmail.com>
2021-11-12 20:49:42 -0300

    master alterando o login para consultaphp

--- consulta.php
+++ consulta.php
@@ -9,10 +9,45 @@
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     </head>
-<body>
+<body class="fundo-contato">
+        <header>
+            <div class="caixa container-fluid">
+                <img src="imagens/icone.png" alt="Ellax Modas" >  
+                <nav class="navbar navbar-expand-lg navbar-light" style="background-color: rgb(255, 192, 203);" >
+                    <div class="container-fluid">
+                      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                      </button>
+                      <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                          
+                          <li class="nav-item">
+                            <a class="nav-link " aria-current="page" href="loja.html">Loja</a>
+                          </li>
+                          
+                          <li class="nav-item">
+                            <a class="nav-link " href="sobre.html">Quem Somos</a>
+                          </li>
+                          
+                          <li class="nav-item">
+                            <a class="nav-link active" href="formulario.html">Contato</a>
+                          </li>
+              
+                          <li class="nav-item">
+                            <a class="nav-link" href="consulta.php">Login</a>
+                          </li>
+                        </ul>
+                        <span class="navbar-text numero">
+                          📞 Zap: (21) 97699 - 2852
+                        </span>
+                      </div>
+                    </div>
+                  </nav>
+            </div>
+        </header>
 
-    <h1> Resultado da pesquisa </h1>
     <div class="container">
+    <h1> Resultado da pesquisa do banco de dados</h1>
     <table class="horarios-contato">
         <?php
         //http://localhost/pesquisa/consulta.php
@@ -47,6 +82,10 @@
         ?>
     </div>
 
+    <footer>
+        <img src="imagens/icone.png">
+        <p class="copyright">&copy; Copyright Ellax Modas 2019 - 2021</p>
+    </footer>
 </body>
 
 </html>
